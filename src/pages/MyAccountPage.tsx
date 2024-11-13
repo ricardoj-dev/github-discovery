61ffb2e431390c74c1682e940ac490889c201e30
Ricardo <ricardosadeveloper@gmail.com>
2024-11-13 14:34:36 +0000

    [WIP] Bootstrap My Account page.

--- src/pages/MyAccountPage.tsx
+++ src/pages/MyAccountPage.tsx
@@ -1,3 +1,10 @@
+import UpdateInformationForm from "../components/UpdateInformationForm";
+
 export default function MyAccountPage() {
-  return <div>MyAccountPage</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl mb-3">My Account</h1>
+      <UpdateInformationForm />
+    </div>
+  );
 }
