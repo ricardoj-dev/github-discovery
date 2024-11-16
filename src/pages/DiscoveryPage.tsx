3d456537bee1ff6dbb7cb72aa28602ea727d0a19
Ricardo <ricardosadeveloper@gmail.com>
2024-11-16 16:07:03 +0000

    Updated DiscoveryPage component.

--- src/pages/DiscoveryPage.tsx
+++ src/pages/DiscoveryPage.tsx
@@ -1,3 +1,22 @@
+import RepositoriesSection from "@/components/repositories-section";
+import BookmarksSection from "../components/bookmarks-section";
+import TopicsSection from "../components/topics-section";
+import { useRepositoriesContext } from "@/lib/hooks";
+import LoadingSpinner from "@/components/LoadingSpinner";
+
 export default function DiscoveryPage() {
-  return <div>DiscoveryPage</div>;
+  const { isGlobalLoading } = useRepositoriesContext();
+  return (
+    <div className="flex flex-col justify-start min-h-screen bg-gray-100">
+      {isGlobalLoading && (
+        <LoadingSpinner
+          classesContainer="fixed top-0 left-0 w-full h-full bg-gray-700 bg-opacity-50 z-50"
+          classesSpinner="border-black"
+        />
+      )}
+      <BookmarksSection />
+      <TopicsSection />
+      <RepositoriesSection />
+    </div>
+  );
 }
