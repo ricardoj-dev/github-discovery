fffd21a06fd79191e98fc0b3800b748bec2fb643
Ricardo <ricardosadeveloper@gmail.com>
2024-11-16 15:45:33 +0000

    Created Firebase configurations.

---
+++ src/config/firebase.ts
@@ -0,0 +1,10 @@
+import appConstants from "@/lib/constants";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Initialize Firebase
+const app = initializeApp(appConstants.firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
