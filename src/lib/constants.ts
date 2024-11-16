efa97d64733ec56a377a807004205b72703f9da5
Ricardo <ricardosadeveloper@gmail.com>
2024-11-16 15:41:25 +0000

    Updated constants with data from .env file.

---
+++ src/lib/constants.ts
@@ -0,0 +1,16 @@
+const appConstants = {
+  // GitHub
+  GIT_HUB_API_URL: import.meta.env.VITE_GITHUB_ACCESS_TOKEN,
+  // Firebase
+  firebaseConfig: {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  },
+};
+
+export default appConstants;
