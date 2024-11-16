23e92b6fc113fb34ba5ca82ecf68f7428f8a0c0d
Ricardo <ricardosadeveloper@gmail.com>
2024-11-16 15:56:52 +0000

    Created ErrorMessage component.

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,10 @@
+const ErrorMessage = () => {
+  return (
+    <div className="flex-grow flex flex-col items-center justify-center gap-1">
+      <p className="text-xl">Error!</p>
+      <p className="text-md">Something went wrong.</p>
+    </div>
+  );
+};
+
+export default ErrorMessage;
