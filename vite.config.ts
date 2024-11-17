2ad97c365ab68d34afce28ce4e546f175493d88b
Ricardo <ricardosadeveloper@gmail.com>
2024-11-17 23:30:11 +0000

    Setting up vitest & jest-dom.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { configDefaults } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,11 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    exclude: [...configDefaults.exclude, "e2e/*"],
   },
 });
