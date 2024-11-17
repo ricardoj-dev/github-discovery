87548358590282e88828ca5d5b06259861336f4f
Ricardo <ricardosadeveloper@gmail.com>
2024-11-17 20:19:12 +0000

    Updated firestore-client with getUserByUID(), setUserBookmarks(), setUserTopics() & setUserSortOptions() functions.

--- src/lib/clients/firestore-client.ts
+++ src/lib/clients/firestore-client.ts
@@ -1,14 +1,19 @@
 import { db } from "@/config/firebase";
+import { Repository, SortOptionsMap, TopicItem } from "@/types";
 import {
   collection,
   doc,
   DocumentData,
+  getDoc,
   getDocs,
   query,
   QuerySnapshot,
   setDoc,
+  updateDoc,
   where,
 } from "firebase/firestore";
+import { handleError } from "../utils";
+import appConstants from "../constants";
 
 async function addNewUser(
   uid: string,
@@ -19,7 +24,26 @@ async function addNewUser(
     uid,
     username,
     email,
+    bookmarks: [],
+    topics: appConstants.initialTopics,
+    sortOptions: {},
   });
+}
+
+async function getUserByUID(uid: string): Promise<DocumentData | null> {
+  try {
+    const userDocRef = doc(db, "users", uid);
+    const userSnapshot = await getDoc(userDocRef);
+
+    if (userSnapshot.exists()) {
+      return userSnapshot.data();
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error("Error fetching user by uid: ", error);
+    return null;
+  }
 }
 
 async function getUserByUsername(
@@ -45,10 +69,56 @@ async function getUserByEmail(
   return await getDocs(emailQuery);
 }
 
+async function setUserBookmarks(
+  uid: string,
+  bookmarks: Repository[]
+): Promise<void> {
+  try {
+    const userDocRef = doc(db, "users", uid);
+    await updateDoc(userDocRef, {
+      bookmarks,
+    });
+  } catch (error) {
+    console.error("Error updating user bookmarks: ", error);
+    handleError("Error updating user bookmarks.");
+  }
+}
+
+async function setUserTopics(uid: string, topics: TopicItem[]): Promise<void> {
+  try {
+    const userDocRef = doc(db, "users", uid);
+    await updateDoc(userDocRef, {
+      topics,
+    });
+  } catch (error) {
+    console.error("Error updating user topics: ", error);
+    handleError("Error updating user topics.");
+  }
+}
+
+async function setUserSortOptions(
+  uid: string,
+  sortOptions: SortOptionsMap
+): Promise<void> {
+  try {
+    const userDocRef = doc(db, "users", uid);
+    await updateDoc(userDocRef, {
+      sortOptions,
+    });
+  } catch (error) {
+    console.error("Error updating user sortOptions: ", error);
+    handleError("Error updating user sortOptions.");
+  }
+}
+
 const firestoreClient = {
   addNewUser,
+  getUserByUID,
   getUserByUsername,
   getUserByEmail,
+  setUserBookmarks,
+  setUserTopics,
+  setUserSortOptions,
 };
 
 export default firestoreClient;
