8cf748e742a7bdf176aef8c64677066cb501e971
Ricardo <ricardosadeveloper@gmail.com>
2024-11-17 20:46:28 +0000

    Updated main file.

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,21 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import "./index.css";
+import AuthContextProvider from "./contexts/providers/AuthContextProvider";
 import BookmarksContextProvider from "./contexts/providers/BookmarksContextProvider";
 import RepositoriesContextProvider from "./contexts/providers/RepositoriesContextProvider";
 import TopicsContextProvider from "./contexts/providers/TopicsContextProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <TopicsContextProvider>
-      <RepositoriesContextProvider>
-        <BookmarksContextProvider>
-          <RouterProvider router={router} />
-        </BookmarksContextProvider>
-      </RepositoriesContextProvider>
-    </TopicsContextProvider>
+    <AuthContextProvider>
+      <TopicsContextProvider>
+        <RepositoriesContextProvider>
+          <BookmarksContextProvider>
+            <RouterProvider router={router} />
+          </BookmarksContextProvider>
+        </RepositoriesContextProvider>
+      </TopicsContextProvider>
+    </AuthContextProvider>
   </StrictMode>
 );
