d7fc9bc490543aa6e12d71af93227228f03304bb
Ricardo <ricardosadeveloper@gmail.com>
2024-12-24 15:08:38 +0000

    Moved file to /ui/carousel. Modified component to be a stateless component.

--- src/components/ui/carousel/CarouselItem.tsx
+++ src/components/ui/carousel/CarouselItem.tsx
@@ -1,62 +1,76 @@
-import { cn } from "@/lib/utils";
-import { Repository } from "@/types";
-import IconStar from "./icons/IconStar";
-import IconStarFill from "./icons/IconStarFill";
-import IconUsersAlt from "./icons/IconUsersAlt";
-import React from "react";
-import IconCodeSandbox from "./icons/IconCodeSandbox";
-import IconGithub from "./icons/IconGithub";
-import IconRepoForkedTwentyFour from "./icons/IconRepoForkedTwentyFour";
-import { useBookmarksContext } from "@/lib/hooks";
+import { Repository } from '@/types';
+import IconStar from '@/components/icons/IconStar';
+import IconStarFill from '@/components/icons/IconStarFill';
+import IconUsersAlt from '@/components/icons/IconUsersAlt';
+import React from 'react';
+import IconCodeSandbox from '@/components/icons/IconCodeSandbox';
+import IconGithub from '@/components/icons/IconGithub';
+import IconRepoForkedTwentyFour from '@/components/icons/IconRepoForkedTwentyFour';
 
 type CarouselItemProps = {
-  isFirstElement: boolean;
-  isLastElement: boolean;
-  repository: Repository;
+  id: string;
+  full_name: string;
+  description: string;
+  watchers_count: number;
+  stargazers_count: number;
+  forks_count: number;
+  html_url: string;
+  is_bookmarked?: boolean;
+  onBookmarkClick?: (repository: Repository) => void;
 };
 
 const CarouselItem = ({
-  isFirstElement,
-  isLastElement,
-  repository,
+  id,
+  full_name,
+  description,
+  watchers_count,
+  stargazers_count,
+  forks_count,
+  html_url,
+  is_bookmarked,
+  onBookmarkClick,
 }: CarouselItemProps) => {
-  const { toggleBookmark } = useBookmarksContext();
-
-  const splittedFullName = repository.full_name.split("/");
+  const splittedFullName = full_name.split('/');
 
   const handleStarClick = async (
     e: React.MouseEvent<SVGSVGElement, MouseEvent>
   ) => {
     e.preventDefault();
     e.stopPropagation();
-    await toggleBookmark(repository);
+    onBookmarkClick?.({
+      id,
+      full_name,
+      description,
+      watchers_count,
+      stargazers_count,
+      forks_count,
+      html_url,
+      is_bookmarked,
+    } as Repository);
   };
 
   return (
-    <a href={repository.html_url} target="_blank" className="hover:z-10">
+    <a
+      href={html_url}
+      target="_blank"
+      className="hover:z-10 flex-shrink-0 w-96 first:hover:translate-x-4 last:hover:-translate-x-4 transition-transform"
+    >
       <div
-        key={repository.id}
-        className={cn(
-          "flex flex-col flex-shrink-0 w-96 p-4 bg-white rounded-lg shadow-md transform transition-transform duration-300 hover:scale-110 hover:z-10",
-          null,
-          {
-            "hover:translate-x-4": isFirstElement,
-            "-hover:translate-x-4": isLastElement,
-          }
-        )}
+        key={id}
+        className="flex flex-col p-4 bg-white rounded-lg shadow-md transform transition-transform duration-300 hover:scale-110"
       >
-        <div className="flex justify-between  items-start">
+        <div className="flex justify-between items-start">
           <div className="flex flex-col gap-2 flex-[0_0_85%]">
             <h2 className="text-xl line-clamp-1">
               {splittedFullName[0]}
               <span className="font-bold">/{splittedFullName[1]}</span>
             </h2>
             <p className="text-[10px] text-gray-600 line-clamp-1">
-              {repository.description}
+              {description}
             </p>
           </div>
           <div className="flex items-center justify-end flex-[0_0_15%]">
-            {repository.is_bookmarked ? (
+            {is_bookmarked ? (
               <IconStarFill className="w-4 h-4" onClick={handleStarClick} />
             ) : (
               <IconStar className="w-4 h-4" onClick={handleStarClick} />
@@ -74,17 +88,11 @@ const CarouselItem = ({
               <IconCodeSandbox />
             </DataContainer>
 
-            <DataContainer
-              quantity={repository.stargazers_count}
-              descriptionLabel="Stars"
-            >
+            <DataContainer quantity={stargazers_count} descriptionLabel="Stars">
               <IconStar />
             </DataContainer>
 
-            <DataContainer
-              quantity={repository.forks_count}
-              descriptionLabel="Forks"
-            >
+            <DataContainer quantity={forks_count} descriptionLabel="Forks">
               <IconRepoForkedTwentyFour />
             </DataContainer>
           </div>
