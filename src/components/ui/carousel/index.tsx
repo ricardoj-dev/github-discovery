74c567252e6db361880e1eda91fcca6a86d71724
Ricardo <ricardosadeveloper@gmail.com>
2024-12-25 19:10:26 +0000

    Added CarouselButton to Carousel.

--- src/components/ui/carousel/index.tsx
+++ src/components/ui/carousel/index.tsx
@@ -1,11 +1,99 @@
+import { useEffect, useRef, useState } from 'react';
+import CarouselButton from './CarouselButton';
+
 type CarouselProps = {
   children: React.ReactNode;
 };
 
 const Carousel = ({ children }: CarouselProps) => {
+  const carouselRef = useRef<HTMLDivElement>(null);
+  const [canScrollLeft, setCanScrollLeft] = useState(false);
+  const [canScrollRight, setCanScrollRight] = useState(false);
+
+  const scrollLeft = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollBy({
+        left: -carouselRef.current.offsetWidth,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  const scrollRight = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollBy({
+        left: carouselRef.current.offsetWidth,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  const checkScrollPosition = () => {
+    if (carouselRef.current) {
+      const { scrollLeft, scrollWidth, offsetWidth } = carouselRef.current;
+
+      const canScrollLeft = scrollLeft > 0;
+      const canScrollRight =
+        scrollWidth > offsetWidth && scrollLeft + offsetWidth < scrollWidth - 1;
+
+      setCanScrollLeft(canScrollLeft);
+      setCanScrollRight(canScrollRight);
+    }
+  };
+
+  useEffect(() => {
+    const carousel = carouselRef.current;
+
+    if (!carousel) {
+      return;
+    }
+
+    checkScrollPosition();
+
+    const handleScroll = () => {
+      checkScrollPosition();
+    };
+
+    carousel.addEventListener('scroll', handleScroll);
+
+    const observer = new MutationObserver(() => {
+      checkScrollPosition();
+    });
+
+    observer.observe(carousel, { childList: true });
+
+    return () => {
+      carousel.removeEventListener('scroll', handleScroll);
+      observer.disconnect();
+    };
+  }, [children]);
+
   return (
-    <div className="flex overflow-x-auto py-4 space-x-4 scrollbar-hide">
-      {children}
+    <div className="relative w-full flex items-center">
+      <div className="w-12 flex justify-center">
+        <CarouselButton
+          direction="left"
+          onClick={scrollLeft}
+          isVisible={canScrollLeft}
+          margin="mr-4"
+        />
+      </div>
+
+      <div
+        ref={carouselRef}
+        className="flex overflow-x-auto py-4 space-x-4 scrollbar-hide scroll-smooth"
+      >
+        {children}
+      </div>
+
+      <div className="w-12 flex justify-center">
+        <CarouselButton
+          direction="right"
+          onClick={scrollRight}
+          isVisible={canScrollRight}
+          margin="ml-4"
+        />
+      </div>
     </div>
   );
 };
