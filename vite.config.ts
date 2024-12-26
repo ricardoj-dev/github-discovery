625faab09639b1b0606be7e56aa4aba19473e8e5
Ricardo <ricardosadeveloper@gmail.com>
2024-12-26 17:35:10 +0000

    Updated vite.config.ts with tsconfigPaths().

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,27 @@
-import path, { resolve } from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { configDefaults } from "vitest/config";
+import path, { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.ts",
-    exclude: [...configDefaults.exclude, "e2e/*"],
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    exclude: [...configDefaults.exclude, 'e2e/*'],
   },
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
+        main: resolve(__dirname, 'index.html'),
       },
     },
   },
