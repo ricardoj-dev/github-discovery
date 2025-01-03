e54cc8532a6dd40046f670635c06f76afc314000
Ricardo <ricardosadeveloper@gmail.com>
2025-01-03 15:55:45 +0000

    Removed context providers from main.tsx.

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import router from "./router";
-import "./index.css";
-import AuthContextProvider from "./contexts/providers/AuthContextProvider";
-import BookmarksContextProvider from "./contexts/providers/BookmarksContextProvider";
-import RepositoriesContextProvider from "./contexts/providers/RepositoriesContextProvider";
-import TopicsContextProvider from "./contexts/providers/TopicsContextProvider";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthContextProvider>
-      <TopicsContextProvider>
-        <RepositoriesContextProvider>
-          <BookmarksContextProvider>
-            <RouterProvider router={router} />
-          </BookmarksContextProvider>
-        </RepositoriesContextProvider>
-      </TopicsContextProvider>
-    </AuthContextProvider>
+    <RouterProvider router={router} />
   </StrictMode>
 );
