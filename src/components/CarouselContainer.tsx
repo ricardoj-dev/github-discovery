d4323ec2b91d31b614f352d92eb849e154d2e68f
Ricardo <ricardosadeveloper@gmail.com>
2025-01-03 16:00:28 +0000

    Replaced context by zustand store in CarouselContainer.tsx.

--- src/components/CarouselContainer.tsx
+++ src/components/CarouselContainer.tsx
@@ -1,14 +1,14 @@
-import { useBookmarksContext } from '@/lib/hooks';
 import Carousel from './ui/carousel';
 import RepositoryItem from './ui/carousel/RepositoryItem';
 import { Repository } from '@/types';
+import useBookmarksStore from '@/stores/bookmarksStore';
 
 const CarouselContainer = ({
   repositories,
 }: {
   repositories: Repository[];
 }) => {
-  const { toggleBookmark } = useBookmarksContext();
+  const toggleBookmark = useBookmarksStore().toggleBookmark;
 
   const handleBookmarkToggle = (repository: Repository) => {
     toggleBookmark(repository);
